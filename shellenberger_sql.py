75192f84fc86ebfa0bb1a42e09d9276f56756358
Brandon Shellenberger <brandonshellenberger93@gmail.com>
2024-02-01 22:04:35 -0600

    Making DB, creating tables

---
+++ shellenberger_sql.py
@@ -0,0 +1,37 @@
+import sqlite3
+import pandas as pd
+import pathlib
+
+db_file = pathlib.Path("project.sqlite3")
+
+def create_database():
+    """Function to create a database. Connecting for the first time
+    will create a new database file if it doesn't exist yet.
+    Close the connection after creating the database
+    to avoid locking the file."""
+    try:
+        conn = sqlite3.connect(db_file)
+        conn.close()
+        print("Database created successfully")
+    except sqlite3.Error as e:
+        print("Error creating the database:", e)
+
+def create_tables():
+    """ Function to read and exicute SL statements to create tables."""
+    try:
+        with sqlite3.connect(db_file) as conn:
+            sql_file = pathlib.Path("shellenberger.sql")
+            with open (sql_file, "r") as file:
+                sql_script = file.read()
+            conn.executescript(sql_script)
+            print("Tables created successfully")
+    except sqlite3.Error as e:
+        print("Error creating tables:", e)
+
+def main():
+    """Main Function"""
+    create_database()
+    create_tables()
+
+if __name__ == "__main__":
+    main()
